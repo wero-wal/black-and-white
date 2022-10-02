03417b07a200f7973ad9106975277dc3cb972cff
W. Walczak <weronikawalczak2005@gmail.com>
2022-10-02 19:20:14 +0100

    created classes texture and point

---
+++ BW/BW/Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BW
+{
+    internal class Point
+    {
+        // ----- Enums -----
+
+
+        // ----- Properties -----
+        public int X { get => _x; set => _x = value; }
+        public int Y { get => _y; set => _y = value; }
+
+        // ----- Fields -----
+        private int _x;
+        private int _y;
+
+        // ----- Constructors -----
+
+
+        // ----- Methods -----
+
+    }
+}
---
+++ BW/BW/Texture.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BW
+{
+    internal class Texture
+    {
+        // ----- Properties -----
+        public ConsoleColor ForegroundColor => _foregroundColor;
+        public ConsoleColor BackgroundColor => _backgroundColor;
+        public string[,] Pattern => _pattern;
+        internal Point Size => _size;
+
+        // ----- Fields -----
+        private readonly ConsoleColor _foregroundColor;
+        private readonly ConsoleColor _backgroundColor;
+        private readonly Point _size;
+        private readonly string[,] _pattern;
+
+        // ----- Constructors -----
+        public Texture()
+        {
+
+        }
+        public Texture(string[,] pattern, ConsoleColor backgroundColour, ConsoleColor foregoundColour = ConsoleColor.Gray)
+        {
+            _pattern = pattern;
+            _size = new Point(pattern.GetLength(0), pattern.GetLength(1));
+            _backgroundColor = backgroundColour;
+            _foregroundColor = foregoundColour;
+        }
+
+        // ----- Methods -----
+        public void Draw(Point position)
+        {
+            Console.ForegroundColor = _foregroundColor;
+            Console.BackgroundColor = _backgroundColor;
+            for (int y = 0; y < _size.Y; y++)
+            {
+                Console.CursorTop = position.Y + _size.Y;
+                for (int x = 0; x < _size.X; x++)
+                {
+                    Console.CursorLeft = position.X + _size.X;
+                    Console.Write(_pattern[x, y]);
+                }
+            }
+        }
+    }
+}
